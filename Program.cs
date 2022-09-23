b10b2764c2962cc1859ee331dbeb8d17d2dc6742
jmgallen <35116410+jmgallen@users.noreply.github.com>
2022-09-22 20:14:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// Jeremy Gallen's sample program to demonstrate polymorphism
+
+// Polymorphism namespace
+namespace Polymorphism
+{
+    // Hero class
+    public class Hero
+    {
+        // Attack method
+        public virtual void attack()
+        {
+            Console.WriteLine("Attacking with equipped weapon...");
+        }
+    }
+
+    // Derived Druid class
+    public class Druid : Hero
+    {
+        // Override attack method
+        public override void attack()
+        {
+            Console.WriteLine("Druid summons animal to attack...");
+        }
+    }
+
+    // Main program class
+    internal class Program
+    {
+        // Main function
+        static void Main(string[] args)
+        {
+            // Declare hero object
+            Hero Jeremy = new Druid();
+
+            // Have hero attack
+            Jeremy.attack();
+        }
+    }
+}
